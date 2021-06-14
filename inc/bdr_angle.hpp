0c21aacb2577cfc364e30ca9d2ce9a6e491a497c
Ahmed Dhahri <ahmed.dhahri17@gmail.com>
2021-06-14 01:30:20 +0100

    Delete inc directory

--- inc/bdr_angle.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once 
-#include "utils.hpp"
-#define PI	3.14159265358979323846
-
-
-
-void sym_angle(pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud, std::vector<Point_info>& pi_array, float tolerance);
-
-double max_angle(pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud, Point_info& pi);
--- inc/bdr_weighted_avg.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once 
-#include "utils.hpp"
-
-
-void sym_weighted_avg(pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud, std::vector<Point_info>& neighborhood, float tolerance);
-
